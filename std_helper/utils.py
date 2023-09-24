e7e3530c7235aed734d9586d093f456a19a6a435
Berkay-Ates <atesberkay2356@gmail.com>
2023-09-25 02:42:10 +0300

    schedule jobs path fixed5

--- std_helper/utils.py
+++ std_helper/utils.py
@@ -75,7 +75,7 @@ def send_user_lesson_mail():
      
     for lesson in lessons:
         lesson_time = lesson.lesson_hour.hour * 60 + lesson.lesson_hour.minute
-        if((lesson_time-current_time) /60 <= 1 and lesson_time >= current_time_minutes):
+        if((lesson_time-current_time_minutes) /60 <= 1 and lesson_time >= current_time_minutes):
             user = Users.objects.get(std_id=lesson.user.std_id)
             message = lesson.lessons_name + ' dersi ' + lesson.class_room + ' sinifinda basliyor'
             send_lesson_mail(target=[user.mail],message=message,lesson_name=lesson.lessons_name)
